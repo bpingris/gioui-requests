284f1bfa69bdc79068a29e38f93cede9333ac689
Jaroslavs Samcuks <yarcat@gmail.com>
2021-02-21 22:41:16 +0100

    utils: use rand.Intn

--- utils/rand.go
+++ utils/rand.go
@@ -7,7 +7,7 @@ const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 func RandString(n int) string {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
 }
