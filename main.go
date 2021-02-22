2daa39cf53225ad2f6ab539ec4a52a774c66d9af
Jaroslavs Samcuks <yarcat@gmail.com>
2021-02-22 14:59:16 +0100

    add request storage demo

--- main.go
+++ main.go
@@ -32,16 +32,13 @@ func loop(w *app.Window) error {
 
 	th := material.NewTheme(gofont.Collection())
 
-	// Home state. We haven't wrapped it with anything since it's the only value so far.
-	requests := state.Requests{
-		state.Request{Method: state.GET, URL: "https://typicode.jsonplaceholder.com/todos/1", Name: "jsonplaceholder"},
-		state.Request{Method: state.POST, URL: "https://typicode.jsonplaceholder.com/comments/1", Name: "/comments"},
-	}
-	save := func(r state.Request) { requests = append(requests, r) }
+	var requests requestStorage
+	requests.add(state.GET, "https://typicode.jsonplaceholder.com/todos/1", "jsonplaceholder")
+	requests.add(state.POST, "https://typicode.jsonplaceholder.com/comments/1", "/comments")
 
 	response := "Last response N/A"
 
-	home := views.HomeScreen(th, fetch, save)
+	home := views.HomeScreen(th, fetch, (*homeScreenRequestStorageAdaptor)(&requests))
 
 	var ops op.Ops
 	for {
@@ -53,7 +50,7 @@ func loop(w *app.Window) error {
 			case system.FrameEvent:
 				gtx := layout.NewContext(&ops, e)
 				fetching := fetchResponse != nil
-				home.Layout(gtx, requests, fetching, response)
+				home.Layout(gtx, fetching, response)
 				e.Frame(gtx.Ops)
 			}
 		case response = <-fetchResponse:
@@ -87,4 +84,30 @@ func (f *fetcher) fetch(r state.Request) string {
 	resp := fmt.Sprintf("Response #%d", f.cnt)
 	log.Printf("Fetched %d bytes", len([]byte(resp)))
 	return resp
+}
+
+// requestStorage and requestProviderAdaptor exist for demonstration purpose.
+type requestStorage struct{ requests state.Requests }
+
+func (rs *requestStorage) add(m state.Method, url, name string) {
+	rs.requests = append(rs.requests, state.Request{
+		Method: m,
+		URL:    url,
+		Name:   name,
+	})
+}
+
+func (rs *requestStorage) addRequest(r state.Request) {
+	rs.requests = append(rs.requests, r)
+}
+
+// homeScreenRequestStorageAdaptor and requestStorage exist for demonstration purpose.
+type homeScreenRequestStorageAdaptor requestStorage
+
+func (rp *homeScreenRequestStorageAdaptor) All() state.Requests {
+	return (*requestStorage)(rp).requests
+}
+
+func (rp *homeScreenRequestStorageAdaptor) Save(r state.Request) {
+	(*requestStorage)(rp).addRequest(r)
 }
--- views/home_screen.go
+++ views/home_screen.go
@@ -8,15 +8,22 @@ import (
 	"gioui.org/widget/material"
 )
 
+// requestStorage is HomeScreen -expected interface. It exists for as a
+// demonstration and will require an adaptor.
+type requestStorage interface {
+	All() state.Requests
+	Save(state.Request)
+}
+
 type HomeScreenStyle struct {
 	url, name         *widget.Editor
 	fetchBtn, saveBtn *widget.Clickable
 	home              HomeStyle
 	fetch             func(url string)
-	save              func(state.Request)
+	reqProv           requestStorage
 }
 
-func HomeScreen(th *material.Theme, fetch func(url string), save func(state.Request)) HomeScreenStyle {
+func HomeScreen(th *material.Theme, fetch func(url string), rp requestStorage) HomeScreenStyle {
 	url := new(widget.Editor)
 	name := new(widget.Editor)
 	fetchBtn := new(widget.Clickable)
@@ -28,20 +35,20 @@ func HomeScreen(th *material.Theme, fetch func(url string), save func(state.Requ
 		saveBtn:  saveBtn,
 		home:     Home(th, url, name, fetchBtn, saveBtn),
 		fetch:    fetch,
-		save:     save,
+		reqProv:  rp,
 	}
 }
 
-func (h HomeScreenStyle) Layout(gtx layout.Context, r state.Requests, fetching bool, response string) layout.Dimensions {
+func (h HomeScreenStyle) Layout(gtx layout.Context, fetching bool, response string) layout.Dimensions {
 	if h.fetchBtn.Clicked() {
 		h.fetch(h.url.Text())
 	}
 	if h.saveBtn.Clicked() {
-		h.save(state.Request{
+		h.reqProv.Save(state.Request{
 			Method: state.GET, // TODO: Change this.
 			URL:    h.url.Text(),
 			Name:   h.name.Text(),
 		})
 	}
-	return h.home.Layout(gtx, r, fetching, response)
+	return h.home.Layout(gtx, h.reqProv.All(), fetching, response)
 }
