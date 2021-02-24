6d72f6f3e7f3d5652c561d515f1f0552330194df
Jaroslavs Samcuks <yarcat@gmail.com>
2021-02-24 15:57:53 +0100

    widget/material: move Home from views

---
+++ widget/material/home.go
@@ -0,0 +1,194 @@
+package material
+
+import (
+	"fmt"
+	"sandbox/state"
+	"strings"
+
+	"gioui.org/layout"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+// requestStorage is HomeScreen -expected interface. It exists for as a
+// demonstration and will require an adaptor.
+type requestStorage interface {
+	All() state.Requests
+	Save(state.Request)
+	Current() state.Request
+	SetCurrent(i int)
+}
+
+type homeStyleState struct {
+	URL, Name   widget.Editor
+	Fetch, Save widget.Clickable
+	Items       []*widget.Clickable
+	ItemsLayout []material.ButtonStyle // Cached Items buttons.
+	btnStyle    material.ButtonStyle   // To create new buttons only.
+}
+
+func (w *homeStyleState) saveRequest(rs requestStorage) {
+	r := state.Request{
+		Method: state.GET, // TODO: Change this.
+		URL:    w.URL.Text(),
+		Name:   w.Name.Text(),
+	}
+	rs.Save(r)
+	w.addSavedRequestButton(r)
+}
+
+func (w *homeStyleState) addSavedRequestButton(r state.Request) {
+	clk := new(widget.Clickable)
+	w.Items = append(w.Items, clk)
+	txt := fmt.Sprintf("%s %s", r.Method, r.Name)
+	btn := w.btnStyle
+	btn.Button = clk
+	btn.Text = txt
+	w.ItemsLayout = append(w.ItemsLayout, btn)
+}
+
+type HomeStyle struct {
+	widgets *homeStyleState
+	home    homeLayoutStyle
+	fetch   func(url string)
+	reqStor requestStorage
+}
+
+func Home(th *material.Theme, fetch func(url string), rs requestStorage) HomeStyle {
+	widgets := &homeStyleState{
+		btnStyle: material.Button(th, nil, ""), // Store as a style only.
+	}
+	widgets.URL.SetText(rs.Current().URL)
+	widgets.Name.SetText(rs.Current().Name)
+	for _, r := range rs.All() {
+		widgets.addSavedRequestButton(r)
+	}
+	return HomeStyle{
+		widgets: widgets,
+		home:    homeLayout(th, widgets),
+		fetch:   fetch,
+		reqStor: rs,
+	}
+}
+
+func (h HomeStyle) Layout(gtx layout.Context, fetching bool, response string) layout.Dimensions {
+	if h.widgets.Fetch.Clicked() {
+		h.fetch(h.widgets.URL.Text())
+	}
+	if h.widgets.Save.Clicked() {
+		h.widgets.saveRequest(h.reqStor)
+	}
+	for i, c := range h.widgets.Items {
+		if c.Clicked() {
+			h.reqStor.SetCurrent(i)
+			h.widgets.URL.SetText(h.reqStor.Current().URL)
+			h.widgets.Name.SetText(h.reqStor.Current().Name)
+		}
+	}
+	return h.home.Layout(gtx, homeLayoutStyleContext{
+		req:      h.reqStor.Current(),
+		fetching: fetching,
+		response: response,
+		saved:    h.widgets.ItemsLayout,
+	})
+}
+
+type homeLayoutStyle struct {
+	loader material.LoaderStyle
+	lbl    material.LabelStyle
+
+	url, name InputStyle
+
+	fetchStyle, saveStyle material.ButtonStyle
+}
+
+func homeLayout(th *material.Theme, state *homeStyleState) homeLayoutStyle {
+	return homeLayoutStyle{
+		loader: material.Loader(th),
+		lbl:    material.Body1(th, ""),
+
+		url:  Input(th, &state.URL, "URL"),
+		name: Input(th, &state.Name, "Name"),
+
+		fetchStyle: material.Button(th, &state.Fetch, "Fetch"),
+		saveStyle:  material.Button(th, &state.Save, "Save"),
+	}
+}
+
+type homeLayoutStyleContext struct {
+	req      state.Request
+	fetching bool
+	response string
+	saved    []material.ButtonStyle
+}
+
+func (h homeLayoutStyle) Layout(gtx layout.Context, ctx homeLayoutStyleContext) layout.Dimensions {
+	homeLayout := func(gtx layout.Context) layout.Dimensions {
+		if ctx.fetching {
+			gtx = gtx.Disabled()
+		}
+		return h.layout(gtx, ctx)
+	}
+	if !ctx.fetching {
+		return homeLayout(gtx)
+	}
+	min := gtx.Constraints.Min
+	return layout.Stack{}.Layout(gtx,
+		layout.Stacked(homeLayout),
+		layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+			gtx.Constraints.Min = min
+			return layout.Center.Layout(gtx, h.loader.Layout)
+		}),
+	)
+}
+
+func (h homeLayoutStyle) layout(gtx layout.Context, ctx homeLayoutStyleContext) layout.Dimensions {
+	methods := func(gtx layout.Context) layout.Dimensions {
+		list := layout.List{Axis: layout.Vertical}
+		return list.Layout(gtx, len(ctx.saved), func(gtx layout.Context, index int) layout.Dimensions {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, ctx.saved[index].Layout)
+		})
+	}
+	inset := func(w layout.Widget) layout.Widget {
+		return func(gtx layout.Context) layout.Dimensions {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, w)
+		}
+	}
+	inputs := func(gtx layout.Context) layout.Dimensions {
+		return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+			layout.Rigid(inset(h.url.Layout)),
+			layout.Rigid(inset(h.name.Layout)),
+		)
+	}
+	enableIf := func(w layout.Widget, enable bool) layout.Widget {
+		return func(gtx layout.Context) layout.Dimensions {
+			if !enable {
+				gtx = gtx.Disabled()
+			}
+			return w(gtx)
+		}
+	}
+	buttons := func(gtx layout.Context) layout.Dimensions {
+		hasURL := len(strings.TrimSpace(h.url.Editor.Editor.Text())) > 0
+		hasName := len(strings.TrimSpace(h.name.Editor.Editor.Text())) > 0
+		return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceStart}.Layout(gtx,
+			layout.Rigid(enableIf(inset(h.fetchStyle.Layout), hasURL)),
+			layout.Rigid(enableIf(inset(h.saveStyle.Layout), hasName)),
+		)
+	}
+	// TODO: May require different style, in which case move it to the constructor.
+	resp := h.lbl
+	resp.Text = ctx.response
+	controls := func(gtx layout.Context) layout.Dimensions {
+		return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+			layout.Rigid(inputs),
+			layout.Rigid(buttons),
+			layout.Rigid(inset(resp.Layout)),
+		)
+	}
+	return layout.Flex{}.Layout(gtx,
+		layout.Flexed(1, methods),
+		layout.Flexed(2, controls),
+	)
+}
