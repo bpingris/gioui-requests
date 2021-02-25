0d44b3b47e7dec04ee5a8e615b3abb1c9b914c3e
Jaroslavs Samcuks <yarcat@gmail.com>
2021-02-25 20:11:39 +0100

    widget/material: clip input area

--- widget/material/input.go
+++ widget/material/input.go
@@ -4,6 +4,8 @@ import (
 	"image/color"
 
 	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
@@ -19,6 +21,10 @@ func (inp InputStyle) Layout(gtx layout.Context) layout.Dimensions {
 	// TODO: These values should be moved to InputStyle.
 	border := widget.Border{Color: color.NRGBA{A: 200}, CornerRadius: unit.Dp(3), Width: unit.Px(1)}
 	return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return layout.UniformInset(unit.Dp(8)).Layout(gtx, inp.EditorStyle.Layout)
+		return layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			defer op.Save(gtx.Ops).Load()
+			clip.Rect{Max: gtx.Constraints.Max}.Add(gtx.Ops)
+			return inp.EditorStyle.Layout(gtx)
+		})
 	})
 }
